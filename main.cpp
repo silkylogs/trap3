cabe236563ceb6661e5b51f9a7ec40e78fde6814
silkylogs <87656538+silkylogs@users.noreply.github.com>
2021-11-16 14:13:27 +0600

    Update main.cpp
    Moved functionality for intended for variableid.cpp to variableid.cpp, thus cleaning up VariableReplacementPass() greatly
    Added operator replacement functionality
    Added a "press enter to exit" mechanic in case users insist on executing the trap3 executable directly through the file manager (meaning, executing with no arguements)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 /*******************************************************************************
 TODO:
--	Debug VariableEnclosingPass()
--	Move concerned functions in VariableId.cpp and main.cpp to another file
-	dedicated for string utilities
--	fix the incrementing condition of EncapsulatePythonVars
--	As per the new paradigm, do the operator replacement pass
+-	Fix array declaration
+-	Write some actual documentation for this language
 
 program function
 - pass 1: converting semantics to pseudocode
@@ -109,249 +106,15 @@ void PseudocodeSyntaxConversionPass()
 
 /******************************************************************************/
 
-void MakeDeclaredVarIds(std::vector<VariableId> &declaredVarIds)
-{
-	int lineNo = 1;
-	std::string tempLine;
-
-	OpenInputFile(srcFileName, sourceFile);
-	while(std::getline(sourceFile, tempLine))
-	{
-		if(tempLine.find("DECLARE") != std::string::npos)
-		{
-			DetectAddDeclaredVariable(declaredVarIds, tempLine, lineNo);
-		}
-		//declaredVarIds.front().PrintDetails();
-		lineNo++;
-	}
-	CloseInputFile(sourceFile);
-	return;
-}
-
-void MakeDetectedIds(std::vector<VariableId> &detectedIds)
-{
-	std::string tempLine;
-	int lineNo = 1;
-
-	OpenInputFile(srcFileName, sourceFile);
-	while(std::getline(sourceFile, tempLine))
-	{
-		//std::cout << "processing line " << lineNo << "\n";
-		std::vector<VariableId> temp;
-
-		//associate line number with each VariableId
-		temp = DetectNamesInLine(tempLine);
-		for(int i = 0; i < temp.size(); i++)
-		{
-			temp[i].lineNumber = lineNo;
-			//std::cout << "details of var inserted:\n";
-			//temp[i].PrintDetails();
-		}
-
-		//append the batch to detectedIds
-		detectedIds.insert
-		(
-			detectedIds.end(),
-			temp.begin(),
-			temp.end()
-		);
-
-		lineNo++;
-	}
-	CloseInputFile(sourceFile);
-	return;
-}
-
-
-//this generates the true list of variable ids
-//note: this may fail if number of detected IDs < declared IDs
-//hence check who is bigger before deciding to assign
-void MakeUtilisedVarIds
-(
-	const std::vector<VariableId> &declaredVarIds,
-	const std::vector<VariableId> &detectedIds,
-	std::vector<VariableId> &utilisedVarIds
-)
-{
-	for(int j = 0; j < detectedIds.size(); j++)
-	{
-		VariableId tVarId;
-
-		//look up each declaredVarId variable name
-		//find how many times it has been used in the script
-		//for each time it has been used, put a duplicate one in utilisedVarIds
-		//the exception being the line number
-		for(int i = 0; i < declaredVarIds.size(); i++)
-		{
-			if(declaredVarIds[i].variableName == detectedIds[j].variableName)
-			{
-				tVarId.variableName = declaredVarIds[i].variableName;
-				tVarId.variableDataType = declaredVarIds[i].variableDataType;
-				tVarId.lineNumber = detectedIds[j].lineNumber;
-
-				utilisedVarIds.insert(utilisedVarIds.begin(), tVarId);
-
-				//std::cout << "inserted var in utilisedVarIds with details: ";
-				//tVarId.PrintDetails();
-			}
-		}
-	}
-
-	return;
-}
-
-void ExtractLineNoFromVarIdVector
-(
-	std::vector<int> &lineNoVector,
-	const std::vector<VariableId> &varIdVector
-)
-{
-	for(int i = 0; i < varIdVector.size(); i++)
-	{
-		lineNoVector.insert
-		(
-			lineNoVector.begin(),
-			varIdVector[i].lineNumber
-		);
-		//std::cout << "inserted line " << varIdVector[i].lineNumber << "\n";
-	}
-	std::sort(lineNoVector.begin(), lineNoVector.end());
-	return;
-}
-
-void AppendPythonDeclaredDatatype
-(
-	const std::vector<VariableId> &declaredVarIds,
-	const std::vector<int> &decVarLineNos
-)
-{
-	std::vector<std::string> tempOutputFileLines;
-
-	ReadTxtFileContentToVector(tempOutputFileLines, outFile, outFileName);
-	int decVarLineIndex = 0;
-	for(int lineIndex = 0; lineIndex < tempOutputFileLines.size(); lineIndex++)
-	{
-		if((lineIndex + 1) == decVarLineNos[decVarLineIndex])
-		{
-			std::string vNameTemp = tempOutputFileLines[lineIndex];
-
-			//strip the variable name from the statement
-			RemoveLeadingIndent(vNameTemp);
-			RemoveUnwantedCharsFromLine(vNameTemp, "=");
-			ExcessSpaceRemover(vNameTemp);
-			vNameTemp.pop_back();
-
-			//search for the matching VariableID
-			for(int dVarInd = 0; dVarInd < declaredVarIds.size(); dVarInd++)
-			{
-				//Append the datatype
-				if(declaredVarIds[dVarInd].variableName == vNameTemp)
-				{
-					tempOutputFileLines[lineIndex] +=
-					EquivalentPythonVar(declaredVarIds[dVarInd]);
-					break;
-				}
-			}
-
-			decVarLineIndex++;
-		}
-
-		//std::cout << "[" << lineIndex + 1 << "]: "
-		//<< tempOutputFileLines[lineIndex] << "\n";
-	}
-	WriteStringVectorToFile(tempOutputFileLines, outFile, outFileName);
-	return;
-}
-
-void EncapsulatePythonVars
-(
-	const std::vector<VariableId> &utilisedVarIds,
-	const std::vector<int> &utilVarLineNos
-)
-{
-	/*
-	Algorithm executed each line specified in utilVarLineNos:
-
-	1. strip indentation
-	2. strip unwanted characters
-	3. strip excess spaces
-	4. get names into a vector
-	5. store those pythonNames into a vector of VariableIDs
-	(with their lineNumbers pointing to string index)
-
-	6. for each of the pythonNames:
-		1. compare if pythonName == any element in utilisedVarIds
-		2. if yes then go to the index of the name in the tempOutputFileLine and
-		encapsulate as nessecary
-		3. else continue
-	*/
-
-	std::vector<std::string> tempOutputFileLines;
-	int varLineNoIndex = 0;
-	
-	for(int i = 0; i < utilVarLineNos.size(); i++)
-	{
-		std::cout << utilVarLineNos[i] << "\n";
-	}
-
-	ReadTxtFileContentToVector(tempOutputFileLines, outFile, outFileName);
-
-	for(int lineNo = 1; lineNo <= tempOutputFileLines.size(); lineNo++)
-	{
-		std::cout << "comparing " << lineNo
-		<< " and " << utilVarLineNos[varLineNoIndex] << "\n";
-		
-		if(lineNo == utilVarLineNos[varLineNoIndex])
-		{
-			std::string tempLine = tempOutputFileLines[lineNo];
-
-			RemoveLeadingIndent(tempLine);
-			RemoveUnwantedCharsFromLine(tempLine, unwanted_chars);
-			ExcessSpaceRemover(tempLine);
-			std::cout << "processed line: " << tempLine << "\n";
-
-			//just because of the repeating elements in utilVarLineNos
-			bool isNextLineNoEqualToLast =
-			utilVarLineNos[varLineNoIndex] ==
-			utilVarLineNos[varLineNoIndex + 1];
-			if(isNextLineNoEqualToLast)
-			{
-				while(isNextLineNoEqualToLast)
-				{
-					varLineNoIndex++;
-					bool isNextLineNoEqualToLast =
-					utilVarLineNos[varLineNoIndex] ==
-					utilVarLineNos[varLineNoIndex + 1];
-				}
-			}
-			else
-			{
-				varLineNoIndex++;
-				continue;
-			}
-		}
-
-	}
-
-	WriteStringVectorToFile(tempOutputFileLines, outFile, outFileName);
-
-	return;
-}
-
 void VariableEnclosingPass()
 {
 	std::vector<VariableId> declaredVarIds;	//declared variables
 	std::vector<VariableId> detectedIds;	//names detected in file
 	std::vector<VariableId> utilisedVarIds;	//the correct variables
 	std::string tempLine;
-	int lineNo = 1;
 
-	//ID pass
-	//make a list of variables (and their types) which were declared		(v1)
-	MakeDeclaredVarIds(declaredVarIds);
-
-	//make a list of variables and lineNos which were used in each lines	(v2)
-	MakeDetectedIds(detectedIds);
+	MakeDeclaredVarIds(declaredVarIds, srcFileName, sourceFile);
+	MakeDetectedIds(detectedIds, srcFileName, sourceFile);
 
 	/*
 	at this point in time;
@@ -366,17 +129,7 @@ void VariableEnclosingPass()
 	std::vector<int> decVarLineNos;
 	ExtractLineNoFromVarIdVector(decVarLineNos, declaredVarIds);
 
-	AppendPythonDeclaredDatatype(declaredVarIds, decVarLineNos);
-
-	/*
-	for each variable, using the contents of utilisedVarIds, go to each line and
-	encapsulate each utilised variable with its type
-	*/
-	std::vector<int> utilVarLineNos;
-	ExtractLineNoFromVarIdVector(utilVarLineNos, utilisedVarIds);
-	
-	EncapsulatePythonVars(utilisedVarIds, utilVarLineNos);
-
+	AppendPythonDeclaredDatatype(declaredVarIds, decVarLineNos, outFileName, outFile);
 
 	std::cout << "\n";
 	std::cout << "Variable enclosing complete.\n";
@@ -387,9 +140,54 @@ void VariableEnclosingPass()
 
 void OperatorReplacementPass()
 {
-	OpenBothFiles();
+	/*
+	substitutions in the out file:
+	= == (only when unindented statement has if or elif)
+	<> !=
+	AND and
+	OR or
+	NOT not
+	*/
 
-	CloseBothFiles();
+	std::vector<std::string> outputLines;
+	ReadTxtFileContentToVector(outputLines, outFile, outFileName);
+
+	for(size_t i = 0; i < outputLines.size(); i++)
+	{
+		std::string moddedLine = outputLines[i];
+
+		if
+		(
+			moddedLine.find("if") != std::string::npos ||
+			moddedLine.find("elif") != std::string::npos
+		)
+		{
+			if(moddedLine.find("=") != std::string::npos)
+			{
+				moddedLine.replace(moddedLine.find("="), sizeof("="), "== ");
+			}
+		}
+		if(moddedLine.find("<>") != std::string::npos)
+		{
+			moddedLine.replace(moddedLine.find("<>"), sizeof("<>"), "!= ");
+		}
+		if(moddedLine.find("AND") != std::string::npos)
+		{
+			moddedLine.replace(moddedLine.find("AND"), sizeof("AND"), "and ");
+		}
+		if(moddedLine.find("OR") != std::string::npos)
+		{
+			moddedLine.replace(moddedLine.find("OR"), sizeof("OR"), "or ");
+		}
+		if(moddedLine.find("NOT") != std::string::npos)
+		{
+			moddedLine.replace(moddedLine.find("NOT"), sizeof("NOT"), "not ");
+		}
+
+		outputLines[i] = moddedLine;
+	}
+
+	WriteStringVectorToFile(outputLines, outFile, outFileName);
 
 	std::cout << "\n";
 	std::cout << "Operator replacement complete.\n";
@@ -414,6 +212,10 @@ void LineCommenter()
 
 int main(int argc, char *argv[])
 {
+	std::cout << "Trap3 cambridge pseudocode to python translator\n" <<
+	"Warning: translation may not be error-proof, " <<
+	"so please do proofread the output if you find any errors\n";
+
 	ArgControl(argc, argv);
 
 	//erase output file first to prevent trailing
@@ -422,6 +224,8 @@ int main(int argc, char *argv[])
 	PseudocodeSyntaxConversionPass();
 	std::cout << "\n";
 	VariableEnclosingPass();
+	std::cout << "\n";
+	OperatorReplacementPass();
 
 	/*
 	do a "press enter to continue mechanic" when there are zero arguments
@@ -432,59 +236,19 @@ int main(int argc, char *argv[])
 	side of things, for this program is getting too bloated as is
 	*/
 	std::cout << "\n\n\n";
-	std::cout << "Complete translation successful.";// Press Enter to exit: ";
-	//std::cin.ignore();
+	std::cout << "Complete translation successful.\n";
+
+
+	/*
+	do a "press enter to continue mechanic" when there are zero arguments
+	say, incase someone actually uses this like some
+	generic double click to run windows application
+	*/
+	if(argc == 1)
+	{
+		std::cout << "Press Enter to exit\n";
+		std::cin.ignore();
+	}
 
 	return 0;
 }
-
-
-
-
-/******************************************************************************/
-//  					"hall" of old code backup							  //
-/******************************************************************************/
-
-/*
-OpenBothFiles();
-	std::cout << "\n";
-
-	std::string sourceLine;
-	int lineIndex = 0;
-	while(std::getline(sourceFile, sourceLine))
-	{
-		std::string outLine = sourceLine;
-		std::cout << "[" << lineIndex + 1 << "]: " << sourceLine << "\n";
-
-		//classify statement
-		OutLineStats olc;
-		olc.AttemptLineClassify(outLine);
-
-		//store variable details (if needed)
-		varInfo[lineIndex].RecordDeclaredVar(olc, outLine);
-		std::cout << "VarName: " <<
-		varInfo[lineIndex].ExtractFirstVarName(olc, outLine) << "\n";
-		//std::cout << "Datatype: " << varInfo[lineIndex].dataType << "\n";
-
-		//comment replacement (best to offload it to a function later)
-		if(outLine.find("//") != std::string::npos)
-		{
-			outLine.replace(outLine.find("//"), strlen("//"), "#");
-		}
-
-		/*
-			if statement contains a variable,
-			but isnt a declaration statment,
-			then enclose it with its type
-		/*
-
-		//modify statement according to classification
-		ConvertLine(olc, varInfo[lineIndex], outLine);
-		std::cout << "Converted: " << outLine << "\n";
-
-		//write modified line to file
-		outFile << outLine + "\n";
-
-		lineIndex++;
-	}
-*/
