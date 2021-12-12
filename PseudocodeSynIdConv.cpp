5cda62393f335105dde8cc89a20412e9842b5dd9
silkylogs <87656538+silkylogs@users.noreply.github.com>
2021-12-12 13:06:59 +0000

    Changed coding style

--- PseudocodeSynIdConv.cpp
+++ PseudocodeSynIdConv.cpp
@@ -4,185 +4,153 @@
 // gcc -c filename.cpp
 // to get the filename.o file
 
-void OutLineStats::DetectMode(std::string &line)
-{
+void OutLineStats::DetectMode(std::string &line){
 	// Note: The checking is unordered in respect to the enum element
 	// sequence because std::line.find() would wrongly detect keywords
 	// otherwise
 
 	// Blank space
-	if(line.empty())
-	{
+	if(line.empty()){
 		mode = BlankLine;
 		return;
 	}
 	// Array declare
-	else if
-	(
+	else if(
 		line.find("DECLARE") != std::string::npos &&
 		line.find(":") != std::string::npos &&
 		line.find("ARRAY") != std::string::npos &&
 		line.find("[") != std::string::npos &&
 		line.find("]") != std::string::npos &&
 		line.find("OF") != std::string::npos
-	)
-	{
+	){
 		mode = ArrayDeclare;
 		return;
 	}
 	// Normal declare
-	else if
-	(
+	else if(
 		line.find("DECLARE") != std::string::npos &&
 		line.find(":") != std::string::npos
-	)
-	{
+	){
 		mode = NormalDeclare;
 		return;
 	}
 	// For loop start
-	else if
-	(
+	else if(
 		line.find("FOR") != std::string::npos &&
 		line.find("TO") != std::string::npos
-	)
-	{
+	){
 		mode = ForLoopStart;
 		return;
 	}
 	// Assignment statement
-	else if(line.find("<-") != std::string::npos)
-	{
+	else if(line.find("<-") != std::string::npos){
 		mode = Assign;
 		return;
 	}
 	// Output
-	else if(line.find("OUTPUT") != std::string::npos)
-	{
+	else if(line.find("OUTPUT") != std::string::npos){
 		mode = TxtOut;
 		return;
 	}
 	// Input
-	else if(line.find("INPUT") != std::string::npos)
-	{
+	else if(line.find("INPUT") != std::string::npos){
 		mode = Input;
 		return;
 	}
 	// While loop start
-	else if
-	(
+	else if(
 		line.find("WHILE") != std::string::npos &&
 		line.find("DO") != std::string::npos
-	)
-	{
+	){
 		mode = WhileLoopStart;
 		return;
 	}
 	// While loop end
-	else if(line.find("ENDWHILE") != std::string::npos)
-	{
+	else if(line.find("ENDWHILE") != std::string::npos){
 		mode = WhileLoopEnd;
 		return;
 	}
 	// For loop end
-	else if(line.find("NEXT") != std::string::npos)
-	{
+	else if(line.find("NEXT") != std::string::npos){
 		mode = ForLoopEnd;
 		return;
 	}
 	// Comment only
-	else if(line.find("//") != std::string::npos)
-	{
+	else if(line.find("//") != std::string::npos){
 		mode = CommentOnly;
 		return;
 	}
 	// Else if keyword
-	else if(line.find("ELSE IF") != std::string::npos)
-	{
+	else if(line.find("ELSE IF") != std::string::npos){
 		mode = ElseIfKeyword;
 		return;
 	}
 	// Endif keyword
-	else if(line.find("ENDIF") != std::string::npos)
-	{
+	else if(line.find("ENDIF") != std::string::npos){
 		mode = IfEnd;
 		return;
 	}
 	// If start
-	else if(line.find("IF") != std::string::npos)
-	{
+	else if(line.find("IF") != std::string::npos){
 		mode = IfStart;
 		return;
 	}
 	// Then keyword
-	else if(line.find("THEN") != std::string::npos)
-	{
+	else if(line.find("THEN") != std::string::npos){
 		mode = ThenKeyword;
 		return;
 	}
 	// Else keyword
-	else if(line.find("ELSE") != std::string::npos)
-	{
+	else if(line.find("ELSE") != std::string::npos){
 		mode = ElseKeyword;
 		return;
 	}
 	// Undefined line
-	else
-	{
+	else{
 		mode = UndefinedLine;
-		std::cout << "lineclassify.h/OutLineStats.DetectMode(): ";
-		std::cout << "Undefined line detected\n";
+		std::cout << "PseudocodeSynIdConv.cpp/OutLineStats.DetectMode()\n";
+		std::cout << "In line: " << line << "\n";
+		std::cout << "Warning: Undefined line detected\n";
 	}
 }
 
-void OutLineStats::DetectIndentLevel(std::string &line)
-{
+void OutLineStats::DetectIndentLevel(std::string &line){
 	int i = 0;
-	while(line[i] == '\t' || line[i] == ' ')
-	{
+	while(line[i] == '\t' || line[i] == ' '){
 		i++;
 		indentLevel++;
 	}
 }
 
-void OutLineStats::AttemptLineClassify(std::string &line)
-{
+void OutLineStats::AttemptLineClassify(std::string &line){
 	DetectIndentLevel(line);
 	DetectMode(line);
 }
 
-void PseudocodeSynConv(OutLineStats &olc, std::string &line)
-{
+void PseudocodeSynConv(OutLineStats &olc, std::string &line){
 	//comment replacement
-	if(line.find("//") != std::string::npos)
-	{
+	if(line.find("//") != std::string::npos){
 		line.replace(line.find("//"), strlen("//"), "#");
 	}
-
-	if(olc.mode == olc.CommentOnly)
-	{
+	if(olc.mode == olc.CommentOnly){
 		return;
 	}
 
-	if(olc.mode == olc.ArrayDeclare)
-	{
+	if(olc.mode == olc.ArrayDeclare){
 		std::string temp;
 		temp.erase();
 
-		line.replace
-		(
+		line.replace(
 			line.find("DECLARE "),
 			strlen("DECLARE "),
 			""
 		);
-		line.replace
-		(
+		line.replace(
 			line.find(":"),
 			strlen(":"),
 			"="
 		);
-		line.erase
-		(
+		line.erase(
 			line.find("ARRAY")
 		);
 
@@ -212,26 +180,21 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		*/
 	}
 
-	else if(olc.mode == olc.NormalDeclare)
-	{
+	else if(olc.mode == olc.NormalDeclare){
 		std::string temp;
 		temp.erase();
 
-		line.replace
-		(
+		line.replace(
 			line.find("DECLARE "),
 			strlen("DECLARE "),
 			""
 		);
-		line.replace
-		(
+		line.replace(
 			line.find(":"),
 			strlen(":"),
 			"= :" //the extra ':' acts as a marker
 		);
-
-		line.erase
-		(
+		line.erase(
 			line.find(":")
 		);
 
@@ -271,20 +234,16 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		*/
 	}
 
-	else if(olc.mode == olc.Assign)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.Assign){
+		line.replace(
 			line.find("<-"),
 			strlen("<-"),
 			"="
 		);
 	}
 
-	else if(olc.mode == olc.TxtOut)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.TxtOut){
+		line.replace(
 			line.find("OUTPUT "),
 			strlen("OUTPUT "),
 			"print("
@@ -292,10 +251,8 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		line += ")";
 	}
 
-	else if(olc.mode == olc.Input)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.Input){
+		line.replace(
 			line.find("INPUT "),
 			strlen("INPUT "),
 			""
@@ -303,39 +260,31 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		line += " = input()";
 	}
 
-	else if(olc.mode == olc.WhileLoopStart)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.WhileLoopStart){
+		line.replace(
 			line.find("WHILE "),
 			strlen("WHILE "),
 			"while "
 		);
-		line.replace
-		(
+		line.replace(
 			line.find(" DO"),
 			strlen(" DO"),
 			":"
 		);
-		
 	}
 
-	else if(olc.mode == olc.ForLoopStart)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.ForLoopStart){
+		line.replace(
 			line.find("FOR "),
 			strlen("FOR "),
 			"for "
 		);
-		line.replace
-		(
+		line.replace(
 			line.find("<-"),
 			strlen("<-"),
 			"in range("
 		);
-		line.replace
-		(
+		line.replace(
 			line.find("TO"),
 			strlen("TO"),
 			","
@@ -343,20 +292,16 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		line.append("):");
 	}
 
-	else if(olc.mode == olc.ForLoopEnd)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.ForLoopEnd){
+		line.replace(
 			line.find("NEXT"),
 			strlen("NEXT"),
 			""
 		);
 	}
 
-	else if(olc.mode == olc.IfStart)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.IfStart){
+		line.replace(
 			line.find("IF "),
 			strlen("IF "),
 			"if "
@@ -364,10 +309,8 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		line.append(":");
 
 		//in case someone decides to write it in the form "IF <condition> THEN"
-		if(line.find("THEN") != std::string::npos)
-		{
-			line.replace
-			(
+		if(line.find("THEN") != std::string::npos){
+			line.replace(
 				line.find("THEN"),
 				strlen("THEN"),
 				""
@@ -375,20 +318,16 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		}
 	}
 
-	else if(olc.mode == olc.ThenKeyword)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.ThenKeyword){
+		line.replace(
 			line.find("THEN"),
 			strlen("THEN"),
 			""
 		);
 	}
 
-	else if(olc.mode == olc.ElseIfKeyword)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.ElseIfKeyword){
+		line.replace(
 			line.find("ELSE IF"),
 			strlen("ELSE IF"),
 			"elif"
@@ -396,42 +335,34 @@ void PseudocodeSynConv(OutLineStats &olc, std::string &line)
 		line.append(":");
 	}
 
-	else if(olc.mode == olc.ElseKeyword)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.ElseKeyword){
+		line.replace(
 			line.find("ELSE"),
 			strlen("ELSE"),
 			"else:"
 		);
 	}
 
-	else if(olc.mode == olc.IfEnd)
-	{
-		line.replace
-		(
+	else if(olc.mode == olc.IfEnd){
+		line.replace(
 			line.find("ENDIF"),
 			strlen("ENDIF"),
 			""
 		);
 	}
 
-	else if(olc.mode == olc.BlankLine)
-	{
-		;
-	}
+	else if(olc.mode == olc.BlankLine) { ; }
 
-	else if(olc.mode == olc.UndefinedLine)
-	{
+	else if(olc.mode == olc.UndefinedLine){
 		/*line.insert(0, "#");
 		line.append(" <--- Undefined line detected here");*/
-		std::cout << "PseudocodeSynIdConv.cpp/ConvertLine() In line:\n"
-		<< line << "\n" << "Warning: line undefined according to pseudocode convention\n";
+		std::cout << "PseudocodeSynIdConv.cpp/ConvertLine()\n" 
+		<< "In line: " << line << "\n"
+		<< "Warning: Converting line known to be undefined according to pseudocode convention\n";
 	}
 
-	else
-	{
-		std::cout << "PseudocodeSynIdConv.cpp/ConvertLine(): ";
+	else{
+		std::cout << "PseudocodeSynIdConv.cpp/ConvertLine():\n";
 		std::cout << "This condition is never to be. \nWhy are you hacking?";
 		exit(1);
 	}
